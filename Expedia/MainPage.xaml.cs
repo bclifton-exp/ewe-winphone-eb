4d8c846ab36c1ed4dfe6af227167ae0a889e2154
bclifton-exp <a-bclifton@expedia.com>
2016-01-21 16:54:59 -0500

    Back button logic hooks for phone navigation, simple temp navigation test before nav service

--- Expedia/MainPage.xaml.cs
+++ Expedia/MainPage.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.Phone.UI.Input;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -16,6 +20,7 @@ using Windows.UI.Xaml.Navigation;
 using Expedia.Client.Contracts;
 using Expedia.Client.Interfaces;
 using Expedia.Client.ViewModels;
+using Expedia.Client.Views;
 using Expedia.Entities.Hotels;
 using Expedia.Entities.Suggestions;
 using Expedia.Entities.User;
@@ -27,51 +32,54 @@ namespace Expedia
 {
     public sealed partial class MainPage : Page
     {
-        public CancellationTokenSource CT { get; set; }
-
         public MainPage()
         {
             this.InitializeComponent();
-            CT = new CancellationTokenSource();
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            currentView.BackRequested += CurrentViewOnBackRequested;
+
+            var isHardwareButtonsApiPresent = ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons");
+            if (isHardwareButtonsApiPresent)
+            {
+                HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+            }
+
+        }
+
+        private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                e.Handled = true;
+                this.Frame.GoBack();
+            }
+            else
+            {
+                Application.Current.Exit();
+            }
+        }
+
+        private void CurrentViewOnBackRequested(object sender, BackRequestedEventArgs backRequestedEventArgs)
+        {
+            if (Frame.CanGoBack)
+            {
+                this.Frame.GoBack();
+            }
+            else
+            {
+                Application.Current.Exit();
+            }
         }
 
         private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            ////pos testing
-            //var posService = ExpediaKernel.Instance().Get<IPointOfSaleService>();
-            //var posResults = posService.GetPointsOfSale();
-            //var posResults2 = posService.SetCurrentPointOfSale(posResults.Result.First().CountryId); //PoS MUST be set before the other services can be called - have to get domain name for URI
-
-            ////testing hotels
-            //var hotelService = ExpediaKernel.Instance().Get<IHotelService>();
-            //var results = await hotelService.GetHotels(new HotelSearchQueryParameters
-            //{ CheckInDate = DateTime.Today, CheckOutDate = DateTime.Today.AddDays(1), City = "SFO", Room = new[] { 1 } }, CT.Token);
-
-            //var hotelCheck = results;
-
-            ////suggestion testing
-            //var suggestion = ExpediaKernel.Instance().Get<ISuggestionService>();
-            //var results2 = await suggestion.Suggest(new CancellationToken(false), "Detroit", SuggestionLob.HOTELS);
-
-            //var suggestCheck = results2;
-
-            ////nearby testing
-            //var results3 = await suggestion.Suggest(new CancellationToken(false), 40.440625, -79.995886, SuggestionLob.HOTELS);
-            //var nearbyCheck = results3;
-
-            //auth testing
-            var authservice = ExpediaKernel.Instance().Get<IAuthenticationService>();
-         
-
-           //account creation
-            var createResult = await authservice.CreateAccount(new CancellationToken(false), new AccountCreationParams("baclifton@gmail.com","apassword","Test","Test",false));
-            var createResult2 = createResult;
-
+            this.Frame.Navigate(typeof (SearchHotelsView));
         }
 
         private void ButtonBase_OnClick1(object sender, RoutedEventArgs e)
         {
-            CT.Cancel();
+            
         }
     }
 }
