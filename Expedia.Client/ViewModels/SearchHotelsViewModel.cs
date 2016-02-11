bca262597d6ddf0701c4512ea2c3e7028d110178
bclifton-exp <a-bclifton@expedia.com>
2016-02-11 12:18:21 -0500

    cleanup for main page resizing

--- Expedia.Client/ViewModels/SearchHotelsViewModel.cs
+++ Expedia.Client/ViewModels/SearchHotelsViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading;
 using System.Windows.Input;
 using Windows.Devices.Geolocation;
+using Windows.Foundation;
 using Windows.UI.Xaml.Controls;
 //using Bing.Maps;
 using Expedia.Client.Interfaces;
@@ -56,29 +58,42 @@ namespace Expedia.Client.ViewModels
 
         private void ExecuteHotelSearch()
         {
-            var check1 = StartDate;
-            var check2 = EndDate;
-
-            var check3 = AdultCount;
-            var check4 = ChildCount;
-
-            var check5 = SelectedSearchSuggestion;
-
-            var check6 = SelectedSearchSuggestion.HotelId;
-
-            var check7 = ChildAges;
+            var childrenAges = ChildAges.Select(c => c.Age).ToArray();
+            double latitude;
+            double.TryParse(SelectedSearchSuggestion.Coordinates.Latitude, out latitude);
+            double longitude;
+            double.TryParse(SelectedSearchSuggestion.Coordinates.Longitude, out longitude);
 
             var hotelSearchParams = new SearchHotelsLocalParameters
             {
-                //LocationName = locationName,
-                //LocationRegionId = regionId,
-                //NearestAirportCode = nearestAirportCode,
-                //CheckInDate = checkInDate,
-                //CheckOutDate = checkOutDate,
-                //AdultsCount = adultsCount,
-                //ChildrenAges = childrenAges,
-                //RoomsCount = roomsCount //this is always one until we have a control to pick
+                LocationName = SelectedSearchSuggestion.RegionNames.ShortName,
+                LocationRegionId = SelectedSearchSuggestion.Id,
+                NearestAirportCode = SelectedSearchSuggestion.HierarchyInfo.Airport?.AirportCode,
+                LocationLatitude = latitude,
+                LocationLongitude = longitude,
+                CheckInDate = StartDate,
+                CheckOutDate = EndDate,
+                AdultsCount = AdultCount,
+                ChildrenAges = childrenAges,
+                RoomsCount = 1 //this is always one until we have a control to pick this value
             };
+
+            if (SelectedSearchSuggestion.HotelId != null)
+            {
+                var hotel = new HotelResultItem { HotelId = SelectedSearchSuggestion.HotelId };
+                DirectHotelSearch(hotel, hotelSearchParams);
+            }
+
+            //if (!double.IsNaN(latitude) && !double.IsNaN(longitude))
+            //{
+            //    hotelSearchParams.LocationLatitude = latitude;
+            //    hotelSearchParams.LocationLongitude = longitude;
+            //}
+        }
+
+        private void DirectHotelSearch(HotelResultItem hotel, SearchHotelsLocalParameters hotelSearchParams)
+        {
+            //TODO this was previously a deeplink to a webview.
         }
     }
 }
