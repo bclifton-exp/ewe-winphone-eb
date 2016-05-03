020ec65749dccee589edb79c658a2ae663af8c29
bclifton-exp <a-bclifton@expedia.com>
2016-05-03 17:13:11 -0400

    polish again

--- Expedia/App.xaml.cs
+++ Expedia/App.xaml.cs
@@ -9,6 +9,9 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -47,7 +50,14 @@ namespace Expedia
         {
             await ExpediaKernel.Instance().Get<ILocationService>().GetSetCurrentLocation();
             TileService.Instance().InitializeLiveTile();
-            
+
+            if (ApiInformation.IsApiContractPresent("Windows.Phone.PhoneContract", 1, 0))
+            {
+                var statusBar = StatusBar.GetForCurrentView();
+                await statusBar.HideAsync();
+                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Portrait;
+            }
+
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
